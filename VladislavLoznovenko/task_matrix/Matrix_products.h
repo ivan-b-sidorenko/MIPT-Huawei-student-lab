4534e105e155424c8ae2d10df35432168de9ff15
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-03-06 20:18:19 +0300

    Added something like vectorization

--- VladislavLoznovenko/task_matrix/Matrix_products.h
+++ VladislavLoznovenko/task_matrix/Matrix_products.h
@@ -26,6 +26,8 @@ Matrix Matrix_product(const Matrix& lhs , const Matrix& rhs)
 	return result;
 }
 
+typedef int v4i __attribute__ ((vector_size (16)));
+
 Matrix Matrix_product_fast(const Matrix& lhs , const Matrix& rhs)
 {
 	int lhs_col = lhs.get_num_col();
@@ -44,9 +46,24 @@ Matrix Matrix_product_fast(const Matrix& lhs , const Matrix& rhs)
 		for (int j = 0 ; j < lhs_col ; ++j)
 		{
 			const int* row_rhs = rhs[j];
+			//Что-то типо векторизации
 			int per_lhs = lhs[i][j];
+			//выравниваем адрес
+			for (int k = 0 ; k < 2 ; ++k)
+				res[k] += row_rhs[k] * per_lhs;
 
-			for (int k = 0 ; k < rhs_col ; ++k)
+			//объединяем данные
+			v4i* res_vec = (v4i*)(res);
+			v4i* row_rhs_vec = (v4i*)(row_rhs);
+
+			//складываем объединённые данные
+			for (int k = 2 ; k < (rhs_col - rhs_col % 4) / 4 ; ++k)
+			{
+				res_vec[k] += row_rhs_vec[k] * per_lhs;
+			}
+
+			//складываем остатки
+			for (int k = (rhs_col - rhs_col % 4) ; k < rhs_col ; ++k)
 				res[k] += row_rhs[k] * per_lhs;
 		}
 	}
