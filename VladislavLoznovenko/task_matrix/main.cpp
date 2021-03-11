e23ef58d365c9d4f5d90f86df9913628350f5205
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-03-11 03:23:47 +0300

    Add checking of correctness of matrix multiplication

--- VladislavLoznovenko/task_matrix/main.cpp
+++ VladislavLoznovenko/task_matrix/main.cpp
@@ -1,27 +1,33 @@
 #include "Matrix.h"
 #include "Matrix_products.h"
 
+#define SIZE 400
+
 using namespace matrix;
 
 int main()
 {
 	uint64_t duration_normal = 0;
 	uint64_t duration_fast = 0;
+	Matrix<int> res_normal(SIZE , SIZE);
+	Matrix<int> res_fast(SIZE , SIZE);
 
 	for (int i = 0 ; i < 5 ; ++i)
 	{
-		Matrix<int> test_1(400 , 400 , 0);
-		Matrix<int> test_2(400 , 400 , 0);
+		Matrix<int> test_1(SIZE , SIZE , 0);
+		Matrix<int> test_2(SIZE , SIZE , 0);
 
 		auto t1 = std::chrono::high_resolution_clock::now();
-		Matrix<int> res = Matrix_product(test_1 , test_2);
+		Matrix_product(test_1 , test_2 , res_normal);
 		auto t2 = std::chrono::high_resolution_clock::now();
 		duration_normal += std::chrono::duration_cast<std::chrono::milliseconds> (t2 - t1).count();
 
 		auto t3 = std::chrono::high_resolution_clock::now();
-		Matrix<int> res_fast = Matrix_product_fast(test_1 , test_2);
+		Matrix_product_fast(test_1 , test_2 , res_fast);
 		auto t4 = std::chrono::high_resolution_clock::now();
 		duration_fast += std::chrono::duration_cast<std::chrono::milliseconds> (t4 - t3).count();
+
+		assert(res_normal == res_fast);
 	}
 
 	std::cout << "Normal product - " << duration_normal / 5 << " - milliseconds" << std::endl;
