bcca62a2df4bd15d7158628f190c1b180c5d952b
Tako-San <dxe13@mail.ru>
2021-03-12 20:54:54 +0300

    1) gitignore 2) some reorg

---
+++ FaridKhaidari/mul.hh
@@ -0,0 +1,44 @@
+#include "matrix.hh"
+
+namespace MUL
+{
+    using namespace MX;
+
+    template <typename DataT>
+    Matrix<DataT> trivial( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
+    {
+        uint lhs_r = lhs.rows(),
+             rhs_r = rhs.rows(),
+             lhs_c = lhs.cols(),
+             rhs_c = rhs.cols();
+        
+        Matrix<DataT> tmp{lhs_r, rhs_c};
+
+        for (uint i = 0; i < lhs_r; ++i)
+            for (uint j = 0; j < rhs_c; ++j)
+                for (uint k = 0; k < lhs_c; ++k)
+                    tmp.set(i, j, lhs[i][k] * rhs[k][j]);
+
+        return tmp;
+    }
+
+    template <typename DataT>
+    Matrix<DataT> transpose( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
+    {
+        uint lhs_r = lhs.rows(),
+             rhs_r = rhs.rows(),
+             lhs_c = lhs.cols(),
+             rhs_c = rhs.cols();
+
+        Matrix<DataT> tmp1{lhs_r, rhs_c};
+        Matrix<DataT> tmp2{transpose(rhs)};
+
+        for (uint i = 0; i < lhs_r; ++i)
+            for (uint j = 0; j < rhs_c; ++j)
+                for (uint k = 0; k < lhs_c; ++k)
+                    tmp1.set(i, j, lhs[i][k] * rhs[j][k]);
+
+        return tmp1;
+    }
+
+} // namespace MUL
