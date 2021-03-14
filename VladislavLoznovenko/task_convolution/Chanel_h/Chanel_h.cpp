3360253acf9b12b9ebfd8c2eae9b5e1875950f95
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-03-14 17:39:15 +0300

    lib for 1 chanel for convolution

---
+++ VladislavLoznovenko/task_convolution/Chanel_h/Chanel_h.cpp
@@ -0,0 +1,90 @@
+#include "Chanel_h.h"
+
+Chanel::Chanel(int num , int num_h , int num_w)
+{
+	high = num_h;
+	width = num_w;
+	num_mrx = num;
+
+	for (int i = 0 ; i < num ; ++i)
+	{
+		chanels.push_back(matrix::Matrix(num_w , num_h));
+	}
+}
+
+Chanel::Chanel(int num , int num_h , int num_w , int mod)
+{
+	high = num_h;
+	width = num_w;
+	num_mrx = num;
+
+	for (int i = 0 ; i < num ; ++i)
+	{
+		chanels.push_back(matrix::Matrix(num_w , num_h , 0));
+	}
+}
+
+Chanel::Chanel(const Chanel& rhs)
+{
+	high = rhs.get_high();
+	width = rhs.get_width();
+	num_mrx = rhs.get_num_mrx();
+
+	if (chanels.size() != 0)
+		chanels.clear();
+
+	for (int i = 0 ; i < num_mrx ; ++i)
+		chanels.push_back(rhs.chanels[i]);
+}
+
+void Chanel::set_zero()
+{
+	for (auto it = chanels.begin() ; it != chanels.end() ; ++it)
+	{
+		it->set_zero();
+	}
+}
+
+bool Chanel::operator==(const Chanel& rhs)
+{
+	if ((num_mrx != rhs.get_num_mrx()) || (high != rhs.get_high()) || (width != rhs.get_width()))
+		return false;
+	for (int i = 0 ; i < num_mrx ; ++i)
+	{
+		if (chanels[i] == rhs.chanels[i])
+			continue;
+		else
+			return false;
+	}
+
+	return true;
+}
+
+Chanel& Chanel::operator=(const Chanel& rhs)
+{
+	if (*this == rhs)
+		return *this;
+
+	high = rhs.get_high();
+	width = rhs.get_width();
+	num_mrx = rhs.get_num_mrx();
+
+	chanels.clear();
+
+	for (int i = 0 ; i < num_mrx ; ++i)
+		chanels.push_back(rhs.chanels[i]);
+
+	return *this;
+}
+
+std::ostream& operator<<(std::ostream& out , const Chanel& rhs)
+{
+	int num = rhs.get_num_mrx();
+	for (int i = 0 ; i < num ; ++i)
+	{
+		std::cout << "Layer - " << i + 1 << std::endl;
+		out << rhs.chanels[i];
+	}
+
+	return out;
+}
---
+++ VladislavLoznovenko/task_convolution/Chanel_h/Chanel_h.h
@@ -0,0 +1,24 @@
+#include "../Matrix_h/Matrix_h.h"
+
+class Chanel final
+{
+private:
+	int num_mrx;
+	int high;
+	int width;
+	std::vector<matrix::Matrix> chanels;
+public:
+	Chanel(int num , int num_h , int num_w);
+	Chanel(int num , int num_h , int num_w , int mod);
+	Chanel(const Chanel& rhs);
+	~Chanel() {};
+
+	int get_num_mrx() const {return num_mrx;};
+	int get_high() const {return high;};
+	int get_width() const {return width;};
+	void set_zero();
+
+	Chanel& operator=(const Chanel& rhs);
+	bool operator==(const Chanel& rhs);
+	friend std::ostream& operator<<(std::ostream& out , const Chanel& rhs);
+};
