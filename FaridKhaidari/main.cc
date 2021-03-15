8d05563c7e7c6c0d764964b8c6f09b3227a1a901
Tako-San <dxe13@mail.ru>
2021-03-15 23:26:29 +0300

    some new ways to multiply

--- FaridKhaidari/main.cc
+++ FaridKhaidari/main.cc
@@ -1,35 +1,87 @@
 #include <iostream>
-#include <cstdint>
 #include <vector>
-#include "matrix.hh"
+#include <cstdio>
+#include <ctime>
 
-using std::cout;
+#include "mul.hh"
+
 using std::cin;
+using std::cout;
 using std::endl;
-
 using std::vector;
+
+void usage( )
+{
+    std::cerr << "USAGE: ./PROGNAME [ALGNUM]" << endl;
+    std::cerr << "0 - trivial, 1 - transpose" << endl;
+}
+
 using namespace MX;
 
-int main ( )
+using Func = Matrix<double> (*) (const Matrix<double> &, const Matrix<double> &);
+
+auto speed_test( const Matrix<double> & m1, const Matrix<double> & m2, Func func )
 {
-  uint size = 0;
-  cin >> size;
+    static uint counter = 0;
 
-  uint el_num = size * size;
+    cout << "Algo" << counter++ << ":" << endl;
+    auto start = std::clock();
 
-  vector<double> raw{};
-  raw.reserve(el_num);
+    Matrix<double> res = (*func)(m1, m2);
+    auto time = static_cast<double>(std::clock() - start) /
+                static_cast<double>(CLOCKS_PER_SEC) * 1000;
 
-  for (int i = 0, end = el_num; i < end; ++i)
-  {
-    double tmp;
-    cin >> tmp;
-    raw.push_back(tmp);
-  }
+    cout << time << " milliseconds" << endl << endl;
 
-  Matrix<double> matr{size, size, raw.begin(), raw.end()};
+    return res;
+}
 
-  cout << matr.det() << endl;
+int main( )
+{
+    uint rows1{}, rows2{},
+         cols1{}, cols2{};
 
-  return 0;
+    cin >> rows1 >> cols1 >> rows2 >> cols2;
+
+    vector<double> raw1{}, raw2{};
+
+    raw1.reserve(rows1 * cols1);
+    
+    for (int i = 0, end = rows1 * cols1; i < end; ++i)
+    {
+        double tmp;
+        cin >> tmp;
+        raw1.push_back(tmp);
+    }
+
+    raw2.reserve(rows2 * cols2);
+    for (int i = 0, end = rows2 * cols2; i < end; ++i)
+    {
+        double tmp;
+        cin >> tmp;
+        raw2.push_back(tmp);
+    }
+
+    Matrix<double> m1{rows1, cols1, raw1.begin(), raw1.end()};
+    Matrix<double> m2{rows2, cols2, raw2.begin(), raw2.end()};
+
+    assert(cols1 == rows2);
+    Matrix<long double> res{rows1, cols2};
+
+    vector<Func> functions{MUL::trivial, 
+                           MUL::transpose,
+
+                           MUL::trivial_cycle2x,
+                           MUL::transpose_cycle2x,
+
+                           MUL::trivial_cycle4x,
+                           MUL::transpose_cycle4x,
+
+                           MUL::trivial_cycle8x,
+                           MUL::transpose_cycle8x};
+
+    for (auto && func : functions)
+        speed_test(m1, m2, func);
+
+    return 0;
 }
