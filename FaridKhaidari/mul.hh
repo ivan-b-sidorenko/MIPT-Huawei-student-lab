8f7c5b16c559ac4510e0cb746aff97e747dcac22
Tako-San <dxe13@mail.ru>
2021-03-18 16:10:59 +0300

    Multiple threads added

--- FaridKhaidari/mul.hh
+++ FaridKhaidari/mul.hh
@@ -1,16 +1,13 @@
 #include <vector>
-//#include <thread>
-//#include <typeinfo>
-//#include <stdexcept>
+#include <thread>
+#include <typeinfo>
+#include <stdexcept>
 
 #include "matrix.hh"
 
 namespace MUL
 {
     using namespace MX;
-
-    const size_t THREADS_NUM = 2;
-
 
     template <typename DataT>
     Matrix<DataT> trivial( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs );
@@ -24,8 +21,8 @@ namespace MUL
     template <typename DataT>
     Matrix<DataT> trivial_cycle8x( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs );
 
-    /*template <typename DataT>
-    Matrix<DataT> trivial_threads( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs );*/
+    template <typename DataT>
+    Matrix<DataT> trivial_threads( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs );
 
 
 
@@ -58,15 +55,17 @@ namespace MUL
     template <typename DataT>
     Matrix<DataT> trivial( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
     {
-        uint lhs_r = lhs.rows(),
-             lhs_c = lhs.cols(),
-             rhs_c = rhs.cols();
+        std::cout << "TRIVIAL" << std::endl;
+
+        size_t lhs_r = lhs.rows(),
+               lhs_c = lhs.cols(),
+               rhs_c = rhs.cols();
         
         Matrix<DataT> tmp{lhs_r, rhs_c};
 
-        for (uint i = 0; i < lhs_r; ++i)
-            for (uint j = 0; j < rhs_c; ++j)
-                for (uint k = 0; k < lhs_c; ++k)
+        for (size_t i = 0; i < lhs_r; ++i)
+            for (size_t j = 0; j < rhs_c; ++j)
+                for (size_t k = 0; k < lhs_c; ++k)
                     tmp.set(i, j, tmp[i][j] + lhs[i][k] * rhs[k][j]);
 
         return tmp;
@@ -75,17 +74,19 @@ namespace MUL
     template <typename DataT>
     Matrix<DataT> trivial_cycle2x( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
     {
-        uint lhs_r = lhs.rows(),
-             lhs_c = lhs.cols(),
-             rhs_c = rhs.cols();
+        std::cout << "TRIVIAL 2X" << std::endl;
+
+        size_t lhs_r = lhs.rows(),
+               lhs_c = lhs.cols(),
+               rhs_c = rhs.cols();
         
         Matrix<DataT> tmp{lhs_r, rhs_c};
 
-        for (uint i = 0; i < lhs_r; ++i)
-            for (uint j = 0; j < rhs_c; ++j)
+        for (size_t i = 0; i < lhs_r; ++i)
+            for (size_t j = 0; j < rhs_c; ++j)
             {
-                uint k = 0;
-                for (uint end = lhs_c - 2; k < end; k += 2)
+                size_t k = 0;
+                for (size_t end = lhs_c - 2; k < end; k += 2)
                     tmp.set(i, j, tmp[i][j] + lhs[i][k] * rhs[k][j]
                                             + lhs[i][k+1] * rhs[k+1][j]);
                 while (k < lhs_c)
@@ -101,17 +102,19 @@ namespace MUL
     template <typename DataT>
     Matrix<DataT> trivial_cycle4x( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
     {
-        uint lhs_r = lhs.rows(),
-             lhs_c = lhs.cols(),
-             rhs_c = rhs.cols();
+        std::cout << "TRIVIAL 4X" << std::endl;
+
+        size_t lhs_r = lhs.rows(),
+               lhs_c = lhs.cols(),
+               rhs_c = rhs.cols();
         
         Matrix<DataT> tmp{lhs_r, rhs_c};
 
-        for (uint i = 0; i < lhs_r; ++i)
-            for (uint j = 0; j < rhs_c; ++j)
+        for (size_t i = 0; i < lhs_r; ++i)
+            for (size_t j = 0; j < rhs_c; ++j)
             {
-                uint k = 0;
-                for (uint end = lhs_c - 4; k < end; k += 4)
+                size_t k = 0;
+                for (size_t end = lhs_c - 4; k < end; k += 4)
                     tmp.set(i, j, tmp[i][j] + lhs[i][k] * rhs[k][j] +
                                               lhs[i][k+1] * rhs[k+1][j] +
                                               lhs[i][k+2] * rhs[k+2][j] +
@@ -129,17 +132,19 @@ namespace MUL
     template <typename DataT>
     Matrix<DataT> trivial_cycle8x( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
     {
-        uint lhs_r = lhs.rows(),
-             lhs_c = lhs.cols(),
-             rhs_c = rhs.cols();
+        std::cout << "TRIVIAL 8X" << std::endl;
+
+        size_t lhs_r = lhs.rows(),
+               lhs_c = lhs.cols(),
+               rhs_c = rhs.cols();
         
         Matrix<DataT> tmp{lhs_r, rhs_c};
 
-        for (uint i = 0; i < lhs_r; ++i)
-            for (uint j = 0; j < rhs_c; ++j)
+        for (size_t i = 0; i < lhs_r; ++i)
+            for (size_t j = 0; j < rhs_c; ++j)
             {
-                uint k = 0;
-                for (uint end = lhs_c - 8; k < end; k += 8)
+                size_t k = 0;
+                for (size_t end = lhs_c - 8; k < end; k += 8)
                     tmp.set(i, j, tmp[i][j] + lhs[i][k] * rhs[k][j] +
                                               lhs[i][k+1] * rhs[k+1][j] +
                                               lhs[i][k+2] * rhs[k+2][j] +
@@ -158,42 +163,40 @@ namespace MUL
         return tmp;
     }
 
-    /*void triv_thread_sub( const Matrix<double> & lhs, 
-                          const Matrix<double> & rhs,
-                          Matrix<double> & tmp,
-                          size_t lhs_beg, 
-                          size_t lhs_end )
+    template <typename DataT>
+    Matrix<DataT> trivial_threads( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
     {
-        uint lhs_r = lhs.rows(),
-             lhs_c = lhs.cols(),
-             rhs_r = rhs.rows(),
-             rhs_c = rhs.cols();
+        std::cout << "TRIVIAL THREADS" << std::endl;
 
-        for (uint i = lhs_beg; i < lhs_end && i < lhs_r; ++i)
-            for (uint j = 0; j < rhs_c; ++j)
-                for (uint k = 0; k < lhs_c; ++k)
-                    tmp.set(i, j, tmp[i][j] + lhs[i][k] * rhs[k][j]);
-    }
+        size_t lhs_r = lhs.rows(),
+               lhs_c = lhs.cols(),
+               rhs_c = rhs.cols();
 
-
-    Matrix<double> trivial_threads( const Matrix<double> & lhs, const Matrix<double> & rhs )
-    {
-        if (typeid(double) != typeid(double))
-            throw std::runtime_error("this function only for double");
+        auto THREADS_NUM = std::thread::hardware_concurrency() - 1;
 
         std::vector<std::thread> threads;
-        size_t NN = lhs.rows() / THREADS_NUM;
+        threads.reserve(THREADS_NUM);
 
-        Matrix<double> tmp{lhs.rows(), rhs.cols()};
+        size_t NN = lhs_r / THREADS_NUM + 1;
+
+        Matrix<DataT> tmp{lhs_r, rhs_c};
+
+        auto f = [&](size_t c)
+        {
+            for (size_t m = c * NN, end = std::min((c + 1)  * NN, lhs_r); m < end; ++m)
+                for (size_t j = 0; j < rhs_c; ++j)
+                    for (size_t k = 0; k < lhs_c; ++k)
+                        tmp.set(m, j, tmp[m][j] + lhs[m][k] * rhs[k][j]);
+        };
 
         for (size_t i = 0; i < THREADS_NUM; ++i)
-            threads.push_back(std::thread(triv_thread_sub, lhs, rhs, tmp, NN * i, NN * (i + 1)));
+            threads.push_back(std::thread(f, i));
 
         for (std::thread & t : threads)
             t.join();
 
         return tmp;
-    }*/
+    }
 
 
 
@@ -208,16 +211,18 @@ namespace MUL
     template <typename DataT>
     Matrix<DataT> transpose( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
     {
-        uint lhs_r = lhs.rows(),
-             lhs_c = lhs.cols(),
-             rhs_c = rhs.cols();
+        std::cout << "TRANSPOSE" << std::endl;
+
+        size_t lhs_r = lhs.rows(),
+               lhs_c = lhs.cols(),
+               rhs_c = rhs.cols();
 
         Matrix<DataT> tmp1{lhs_r, rhs_c};
         Matrix<DataT> tmp2{transpose(rhs)};
 
-        for (uint i = 0; i < lhs_r; ++i)
-            for (uint j = 0; j < rhs_c; ++j)
-                for (uint k = 0; k < lhs_c; ++k)
+        for (size_t i = 0; i < lhs_r; ++i)
+            for (size_t j = 0; j < rhs_c; ++j)
+                for (size_t k = 0; k < lhs_c; ++k)
                     tmp1.set(i, j, tmp1[i][j] + lhs[i][k] * tmp2[j][k]);
 
         return tmp1;
@@ -226,18 +231,20 @@ namespace MUL
     template <typename DataT>
     Matrix<DataT> transpose_cycle2x( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
     {
-        uint lhs_r = lhs.rows(),
-             lhs_c = lhs.cols(),
-             rhs_c = rhs.cols();
+        std::cout << "TRANSPOSE 2X" << std::endl;
+
+        size_t lhs_r = lhs.rows(),
+               lhs_c = lhs.cols(),
+               rhs_c = rhs.cols();
 
         Matrix<DataT> tmp1{lhs_r, rhs_c};
         Matrix<DataT> tmp2{transpose(rhs)};
 
-        for (uint i = 0; i < lhs_r; ++i)
-            for (uint j = 0; j < rhs_c; ++j)
+        for (size_t i = 0; i < lhs_r; ++i)
+            for (size_t j = 0; j < rhs_c; ++j)
             {
-                uint k = 0;
-                for (uint end = lhs_c - 2; k < end; k += 2)
+                size_t k = 0;
+                for (size_t end = lhs_c - 2; k < end; k += 2)
                     tmp1.set(i, j, tmp1[i][j] + lhs[i][k] * tmp2[j][k] + lhs[i][k+1] * tmp2[j][k+1]);
                 while (k < lhs_c)
                 {
@@ -252,18 +259,20 @@ namespace MUL
     template <typename DataT>
     Matrix<DataT> transpose_cycle4x( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
     {
-        uint lhs_r = lhs.rows(),
-             lhs_c = lhs.cols(),
-             rhs_c = rhs.cols();
+        std::cout << "TRANSPOSE 4X" << std::endl;
+
+        size_t lhs_r = lhs.rows(),
+               lhs_c = lhs.cols(),
+               rhs_c = rhs.cols();
 
         Matrix<DataT> tmp1{lhs_r, rhs_c};
         Matrix<DataT> tmp2{transpose(rhs)};
 
-        for (uint i = 0; i < lhs_r; ++i)
-            for (uint j = 0; j < rhs_c; ++j)
+        for (size_t i = 0; i < lhs_r; ++i)
+            for (size_t j = 0; j < rhs_c; ++j)
             {
-                uint k = 0;
-                for (uint end = lhs_c - 4; k < end; k += 4)
+                size_t k = 0;
+                for (size_t end = lhs_c - 4; k < end; k += 4)
                     tmp1.set(i, j, tmp1[i][j] + lhs[i][k] * tmp2[j][k] + 
                                                 lhs[i][k+1] * tmp2[j][k+1] + 
                                                 lhs[i][k+2] * tmp2[j][k+2] + 
@@ -281,18 +290,20 @@ namespace MUL
     template <typename DataT>
     Matrix<DataT> transpose_cycle8x( const Matrix<DataT> & lhs, const Matrix<DataT> & rhs )
     {
-        uint lhs_r = lhs.rows(),
-             lhs_c = lhs.cols(),
-             rhs_c = rhs.cols();
+        std::cout << "TRANSPOSE 8X" << std::endl;
+
+        size_t lhs_r = lhs.rows(),
+               lhs_c = lhs.cols(),
+               rhs_c = rhs.cols();
 
         Matrix<DataT> tmp1{lhs_r, rhs_c};
         Matrix<DataT> tmp2{transpose(rhs)};
 
-        for (uint i = 0; i < lhs_r; ++i)
-            for (uint j = 0; j < rhs_c; ++j)
+        for (size_t i = 0; i < lhs_r; ++i)
+            for (size_t j = 0; j < rhs_c; ++j)
             {
-                uint k = 0;
-                for (uint end = lhs_c - 8; k < end; k += 8)
+                size_t k = 0;
+                for (size_t end = lhs_c - 8; k < end; k += 8)
                     tmp1.set(i, j, tmp1[i][j] + lhs[i][k] * tmp2[j][k] + 
                                                 lhs[i][k+1] * tmp2[j][k+1] + 
                                                 lhs[i][k+2] * tmp2[j][k+2] + 
