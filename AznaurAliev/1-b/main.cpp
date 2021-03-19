7319c8e9da587786cc85994fdda9f585a6ea8168
Aznaur <aliev.ak@phystech.edu>
2021-03-20 01:13:26 +0300

    Tests added

--- AznaurAliev/1-b/main.cpp
+++ AznaurAliev/1-b/main.cpp
@@ -2,28 +2,123 @@
 #include "log_duration.h"
 #include "mat_mult.h"
 
+void TEST();
+
+
 int main()
 {
-	matrix mat_1(300, 150, true);
-	matrix mat_2(150, 500, true);
 
-	matrix simple(300, 500);
-	matrix modified(300, 500);
-	
-
-	{
-		// Simple Multiplication part
-		LOG_DURATION("Simple Multiplication");
-		Naive_mult(mat_1, mat_2, simple);
-	}
-
-	{	
-		// Modified Multiplication part
-		LOG_DURATION("Modified Multiplication");
-		Modified_mult(mat_1, mat_2, modified);
-	}
-
-	assert(simple == modified);
+	TEST();
 
 	return 0;
+}
+
+
+void TEST(){
+
+	{
+		std::cout << "TEST for 50X30 and 30x50:\n\n";
+
+		matrix mat_1(50, 30, true);
+		matrix mat_2(30, 50, true);
+
+		matrix simple(50, 50);
+		matrix modified(50, 50);
+	
+
+		{
+			// Simple Multiplication part
+			LOG_DURATION("\tSimple Multiplication");
+			Naive_mult(mat_1, mat_2, simple);
+		}
+
+		{		
+			// Modified Multiplication part
+			LOG_DURATION("\tModified Multiplication");
+			Modified_mult(mat_1, mat_2, modified);
+		}	
+
+		assert(simple == modified);
+		cout << "\n";
+	}
+
+	{
+		std::cout << "TEST for 150X300 and 300x500:\n\n";
+
+		matrix mat_1(150, 300, true);
+		matrix mat_2(300, 500, true);
+
+		matrix simple(150, 500);
+		matrix modified(150, 500);
+	
+
+		{
+			// Simple Multiplication part
+			LOG_DURATION("\tSimple Multiplication");
+			Naive_mult(mat_1, mat_2, simple);
+		}
+
+		{		
+			// Modified Multiplication part
+			LOG_DURATION("\tModified Multiplication");
+			Modified_mult(mat_1, mat_2, modified);
+		}	
+
+		assert(simple == modified);
+		cout << "\n";
+	}
+
+	{
+		std::cout << "TEST for 1000X500 and 500x1500:\n\n";
+
+		matrix mat_1(1000, 500, true);
+		matrix mat_2(500, 1000, true);
+
+		matrix simple(1000, 1500);
+		matrix modified(1000, 1500);
+	
+
+		{
+			// Simple Multiplication part
+			LOG_DURATION("\tSimple Multiplication");
+			Naive_mult(mat_1, mat_2, simple);
+		}
+
+		{		
+			// Modified Multiplication part
+			LOG_DURATION("\tModified Multiplication");
+			Modified_mult(mat_1, mat_2, modified);
+		}	
+
+		assert(simple == modified);
+		cout << "\n";	
+	}
+
+
+	{
+		std::cout << "TEST for 2000X750 and 750x980:\n\n";
+
+		matrix mat_1(2000, 750, true);
+		matrix mat_2(750, 980, true);
+
+		matrix simple(2000, 980);
+		matrix modified(2000, 980);
+	
+
+		{
+			// Simple Multiplication part
+			LOG_DURATION("\tSimple Multiplication");
+			Naive_mult(mat_1, mat_2, simple);
+		}
+
+		{		
+			// Modified Multiplication part
+			LOG_DURATION("\tModified Multiplication");
+			Modified_mult(mat_1, mat_2, modified);
+		}	
+
+		assert(simple == modified);
+		cout << "\n";	
+	}
+
 }
--- AznaurAliev/1-b/mat_mult.h
+++ AznaurAliev/1-b/mat_mult.h
@@ -24,66 +24,30 @@ void Modified_mult(const matrix& lhs , const matrix& rhs, matrix& result1)
 {
         assert(lhs.num_col == rhs.num_str);
 
-	int ost = lhs.num_col % 6;
-	const int block = 6;
-	const int reg = 8;
+	const int block = sizeof(__m256i)/sizeof(int);
 
-	if (lhs.num_col < 5){
-		for (int i = 0; i < rhs.num_col; i++){
-			int res[lhs.num_col];
+	for (int i = 0; i < lhs.num_str; i++){
+		
+		__m256i* _cache = (__m256i*)(result1[i]);
 
-			for (int t = 0; t < lhs.num_col; t++){
-	
-				res[t] = rhs[t][i];	
-			}
+		for (int j = 0; j < lhs.num_col; j++){
 
-    			for (int j = 0; j < lhs.num_str; j++){
+			const __m256i* _rhs = (__m256i*)(rhs[j]);
+			const __m256i temp = _mm256_set1_epi32(lhs[i][j]);
 
-				int cache = 0;
-				const int* temp = lhs[j];
-
-				for(int k1 = 0; k1 < lhs.num_col; k1++){
-					cache += temp[k1]*res[k1];
-				}
-				result1[j][i] = cache;
-
-			}
-		}
-
-	}
-
-	else{
-        	for (int i = 0; i < lhs.num_str; i++){
+			for (int k = 0; k < rhs.num_col / block; k++){
 			
-			__m256i* _cache = (__m256i*)(result1[i]);
+				__m256i integer_res = _mm256_loadu_si256(&_cache[k]);
+				__m256i integer_rhs = _mm256_loadu_si256(&_rhs[k]);				
+				_mm256_storeu_si256(&_cache[k], _mm256_add_epi32(_mm256_mullo_epi32(integer_rhs, temp), integer_res));					
+			}
+		
 
-                	for (int j = 0; j < lhs.num_col; j++){
-
-				const __m256i* _rhs = (__m256i*)(rhs[j]);
-				const __m256i temp = _mm256_set1_epi32(lhs[i][j]);
-
-				for (int k = 0; k < rhs.num_col / 8; k++){
-								
-					__m256i mull = _mm256_mullo_epi32(temp , _mm256_loadu_si256(&_rhs[k]));	
-					_mm256_storeu_si256(&_cache[k], _mm256_add_epi32( mull, _mm256_loadu_si256(&_cache[k])));					
-					/*cache[k] = cache[k] + res[k] * temp
-					 *
-					__m256i integer_vector = _mm256_loadu_si256(&cache[k]);
-					__m256i integer_row = _mm256_loadu_si256(&_rhs[k]);
-					__m256i mull = _mm256_mullo_epi32(temp, integer_row);
-					__m256i sum = _mm256_add_epi32(integer_vector, mull);
-					_mm256_storeu_si256(&cache[k], sum); 
-					*/
-				}
-			
-
-				for (int t = rhs.num_col - rhs.num_col % 8; t < rhs.num_col; t++){
-					result1[i][t] += rhs[j][t] * lhs[i][j];
-				}
+			for (int t = rhs.num_col - rhs.num_col % block; t < rhs.num_col; t++){
+				result1[i][t] += rhs[j][t] * lhs[i][j];
 			}
 		}
 	}
-
 
 }
 
