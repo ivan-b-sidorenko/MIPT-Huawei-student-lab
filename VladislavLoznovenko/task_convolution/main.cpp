42202d8539cc3f07d5d7329209a25bc31f7f86a5
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-03-20 23:09:04 +0300

    added time comparison

--- VladislavLoznovenko/task_convolution/main.cpp
+++ VladislavLoznovenko/task_convolution/main.cpp
@@ -1,13 +1,38 @@
-#include "Tensor4_h/Tensor4.h"
+#include "Convolutions_h/Conv_naiv.h"
+#include <chrono>
+#include <thread>
+#include <ratio>
 
+using namespace matrix;
 
 int main()
 {
-	Tensor4 test_1(1 , 5 , 5 , 5);
-	Tensor4 test_2(1 , 4 , 3 , 3 , 0);
-	test_2 = test_1;
-	Tensor4 test_3 = test_2;
-	std::cout << test_1;
-	std::cout << test_2;
+
+	uint64_t duration_normal = 0;
+	uint64_t duration_fast = 0;
+	Tensor4 test(1 , 16 , 100 , 100 , 0);
+	Chanel kernels(16 , 4 , 4 , 0);
+	for (int i = 0 ; i < 5 ; ++i)
+	{
+		auto t1 = std::chrono::high_resolution_clock::now();
+		Tensor4 res_1 = conv_main_algo(test , kernels , 4);
+		auto t2 = std::chrono::high_resolution_clock::now();
+		duration_normal += std::chrono::duration_cast<std::chrono::milliseconds> (t2 - t1).count();
+
+		auto t3 = std::chrono::high_resolution_clock::now();
+		Tensor4 res_2 = conv_main_algo_fast(test , kernels , 4);
+		auto t4 = std::chrono::high_resolution_clock::now();
+		duration_fast += std::chrono::duration_cast<std::chrono::milliseconds> (t4 - t3).count();
+
+		assert(res_1 == res_2);
+	}
+
+	std::cout << "Normal conv - " << duration_normal << " - milliseconds" << std::endl;
+	std::cout << "Fast conv   - " << duration_fast << " - milliseconds" << std::endl;
+
+	/*std::cout << "test\n" << test << std::endl;
+	std::cout << "kernels\n" << kernels << std::endl;*/
+	/*std::cout << "res_1\n" << res_1 << std::endl;
+	std::cout << "res_2\n" << res_2 << std::endl;*/
 	return 0;
 }
