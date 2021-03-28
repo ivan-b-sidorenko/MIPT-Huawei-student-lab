6b788ab85ff08855f47f9c172f700752ffaeb564
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-03-28 22:50:35 +0300

    make it without conv

--- VladislavLoznovenko/task_convolution/Convolutions_h/Conv_fast.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "../Tensor4_h/Tensor4.h"
-
-Tensor4 conv_main_algo_fast(Tensor4 for_conv , Chanel filter , int layers);
-Tensor4 convolution_layer_fast(Tensor4& for_conv , Chanel& filter);
-matrix::Matrix treatment_fast(matrix::Matrix& chanel , matrix::Matrix& kernel);
