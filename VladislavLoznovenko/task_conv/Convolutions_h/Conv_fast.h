fe04123a06493405e1b8abb58422dc4c3e9289dc
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-04-05 19:34:28 +0300

    added GEMM convolution

--- VladislavLoznovenko/task_conv/Convolutions_h/Conv_fast.h
+++ VladislavLoznovenko/task_conv/Convolutions_h/Conv_fast.h
@@ -4,4 +4,6 @@
 using namespace Tensor;
 
 matrix::Matrix GEMM_ker(Tensor4& filter);
-matrix::Matrix GEMM_ten(Tensor4& f_map);
+matrix::Matrix GEMM_ten(Tensor4& f_map , Tensor4& filter);
+void reverse_GEMM(matrix::Matrix& OFmap , Tensor4& result);
+Tensor4 conv_layer_fast(Tensor4& f_map , Tensor4& filter);
--- VladislavLoznovenko/task_conv/main.cpp
+++ VladislavLoznovenko/task_conv/main.cpp
@@ -12,7 +12,7 @@ int main()
 
 	uint64_t duration_normal = 0;
 	uint64_t duration_fast = 0;
-	Tensor4 test(1 , 3 , 10 , 10 , 0);
+	Tensor4 test(1 , 3 , 1000 , 1000 , 0);
 	Tensor4 filter(16 , 3 , 4 , 4 , 0);
 	for (int i = 0 ; i < 5 ; ++i)
 	{
@@ -21,26 +21,21 @@ int main()
 		auto t2 = std::chrono::high_resolution_clock::now();
 		duration_normal += std::chrono::duration_cast<std::chrono::milliseconds> (t2 - t1).count();
 
-		/*auto t3 = std::chrono::high_resolution_clock::now();
-		Tensor4 res_2 = conv_main_algo_fast(test , kernels , 4);
+		auto t3 = std::chrono::high_resolution_clock::now();
+		Tensor4 res_2 = conv_layer_fast(test , filter);
 		auto t4 = std::chrono::high_resolution_clock::now();
 		duration_fast += std::chrono::duration_cast<std::chrono::milliseconds> (t4 - t3).count();
 
-		assert(res_1 == res_2);*/
+		assert(res_1 == res_2);
 	}
 
-	std::cout << "Normal conv - " << duration_normal / 5 << " - milliseconds" << std::endl;
-	//std::cout << "Fast conv   - " << duration_fast << " - milliseconds" << std::endl;
+	std::cout << "Normal conv - " << duration_normal << " - milliseconds" << std::endl;
+	std::cout << "Fast conv   - " << duration_fast << " - milliseconds" << std::endl;
 
 	/*std::cout << "test\n" << test << std::endl;
 	std::cout << "kernels\n" << filter << std::endl;
 	std::cout << "res_1\n" << res_1 << std::endl;
 	std::cout << "res_2\n" << res_2 << std::endl;*/
-	/*Tensor4 filter(3 , 2 , 2 , 2 , 0);
-	std::cout << filter << std::endl;
-
-	Matrix res = GEMM_ker(filter);
-	std::cout << res << std::endl;*/
 
 	return 0;
 }
