2d3e2b802f17a032f76099de2311139e34e02661
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-04-11 01:19:54 +0300

    added class Timer

--- VladislavLoznovenko/task_conv/main.cpp
+++ VladislavLoznovenko/task_conv/main.cpp
@@ -1,9 +1,7 @@
 #include "Convolutions_h/Conv_naiv.h"
 #include "Convolutions_h/Conv_fast.h"
 #include "Convolutions_h/Conv_GEMM.h"
-#include <chrono>
-#include <thread>
-#include <ratio>
+#include "time.h"
 
 using namespace matrix;
 using namespace Tensor;
@@ -14,25 +12,23 @@ int main()
 	uint64_t duration_normal = 0;
 	uint64_t duration_fast = 0;
 	uint64_t duration_GEMM = 0;
+	my_time::Timer clock;
 
 	Tensor4 test(1 , 3 , 100 , 50 , 0);
 	Tensor4 filter(16 , 3 , 4 , 4 , 0);
 	for (int i = 0 ; i < 5 ; ++i)
 	{
-		auto t1 = std::chrono::high_resolution_clock::now();
+		clock.restart();
 		Tensor4 res_1 = convolution_layer_naive(test , filter);
-		auto t2 = std::chrono::high_resolution_clock::now();
-		duration_normal += std::chrono::duration_cast<std::chrono::milliseconds> (t2 - t1).count();
+		duration_normal += clock.get_mls();
 
-		auto t3 = std::chrono::high_resolution_clock::now();
+		clock.restart();
 		Tensor4 res_2 = conv_layer_fast(test , filter);
-		auto t4 = std::chrono::high_resolution_clock::now();
-		duration_fast += std::chrono::duration_cast<std::chrono::milliseconds> (t4 - t3).count();
+		duration_fast += clock.get_mls();
 
-		auto t5 = std::chrono::high_resolution_clock::now();
+		clock.restart();
 		Tensor4 res_3 = conv_layer_GEMM(test , filter);
-		auto t6 = std::chrono::high_resolution_clock::now();
-		duration_GEMM += std::chrono::duration_cast<std::chrono::milliseconds> (t6 - t5).count();
+		duration_GEMM += clock.get_mls();
 
 		assert(res_1 == res_2);
 		assert(res_1 == res_3);
