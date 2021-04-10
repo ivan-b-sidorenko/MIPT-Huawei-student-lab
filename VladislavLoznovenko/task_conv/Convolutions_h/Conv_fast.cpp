075db1a2b22bfaefe5e8f84a3a7edbd42b6a203b
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-04-11 00:12:03 +0300

    added somthing like tiling, but now its slower than naive

--- VladislavLoznovenko/task_conv/Convolutions_h/Conv_fast.cpp
+++ VladislavLoznovenko/task_conv/Convolutions_h/Conv_fast.cpp
@@ -1,85 +1,78 @@
 #include "Conv_fast.h"
 
-matrix::Matrix GEMM_ker(Tensor4& filter)
+Tensor4 conv_layer_fast(Tensor4& for_conv , Tensor4& filter)
 {
-	int data_h = filter.get_high();
-	int data_w = filter.get_width();
-	int data_c = filter.get_num_mrx();
-	int data_b = filter.get_num_butch();
+	int new_h = for_conv.get_high() - filter.get_high() + 1;
+	int new_w = for_conv.get_width() - filter.get_width() + 1;
 
-	int fil_high = data_h * data_w * data_c;
-	int fil_width = data_b;
+	Tensor4 result(for_conv.get_num_butch() , filter.get_num_butch() , new_h , new_w);
 
-	matrix::Matrix filter_matrix(fil_high , fil_width);
+	std::vector<Tensor4> ten_parts = make_parts_for_tile(for_conv , filter.get_high());
 
-	for (int butch = 0 ; butch < data_b ; ++butch)
-		for (int chan = 0 ; chan < data_c ; ++chan)
-			for (int high = 0 ; high < data_h ; ++high)
-				for (int width = 0 ; width < data_w ; ++width)
-					filter_matrix[chan * data_w * data_h + high * data_w + width][butch] = filter[butch][chan][high][width];
+	std::vector<std::thread> threads;
 
-	return filter_matrix;
+	for (int i = 0 ; i < ten_parts.size() / 8 ; ++i)
+	{
+		treat(ten_parts[i * 8 + 0] , result , filter , i * 8 + 0);
+		treat(ten_parts[i * 8 + 1] , result , filter , i * 8 + 1);
+		treat(ten_parts[i * 8 + 2] , result , filter , i * 8 + 2);
+		treat(ten_parts[i * 8 + 3] , result , filter , i * 8 + 3);
+		treat(ten_parts[i * 8 + 4] , result , filter , i * 8 + 4);
+		treat(ten_parts[i * 8 + 5] , result , filter , i * 8 + 5);
+		treat(ten_parts[i * 8 + 6] , result , filter , i * 8 + 6);
+		treat(ten_parts[i * 8 + 7] , result , filter , i * 8 + 7);
+	}
+
+	for (int i = ten_parts.size() - ten_parts.size() % 8 ; i < ten_parts.size() ; ++i)
+		treat(ten_parts[i] , result , filter , i);
+
+	return result;		
 }
 
-matrix::Matrix GEMM_ten(Tensor4& f_map , Tensor4& filter)
+void treat(Tensor4& ten_part , Tensor4& result , Tensor4& filter , int counter)
 {
-	int data_h = f_map.get_high();
-	int data_w = f_map.get_width();
-	int data_c = f_map.get_num_mrx();
-	int data_b = f_map.get_num_butch();
-
-	int fil_h = filter.get_high();
-	int fil_w = filter.get_width();
-
-	int fil_high = data_b * (data_h - fil_h + 1) * (data_w - fil_w + 1);
-	int fil_width = fil_h * fil_w * data_c; 
-
-	matrix::Matrix IFmap_matrix(fil_high , fil_width);
-
-	for (int butch = 0 ; butch < data_b ; ++butch)
-		for (int chan = 0 ; chan < data_c ; ++chan)
-
-			for (int i = 0 ; i < data_h - fil_h + 1 ; ++i)
-				for (int j = 0 ; j < data_w - fil_w + 1 ; ++j)
-
-					for (int start_h = i ; start_h < i + fil_h ; ++start_h)
-						for (int start_w = j ; start_w < j + fil_w ; ++start_w)
-							IFmap_matrix[butch * (data_h - fil_h + 1) * (data_w - fil_w + 1) + i * (data_w - fil_w + 1) + j][chan * fil_h * fil_w  + (start_h - i) * (fil_w) + start_w - j] = f_map[butch][chan][start_h][start_w];
-
-	return IFmap_matrix;
+	Tensor4 per = treatment_fast(ten_part , filter);
+	add_ten(per , result , counter);
 }
 
-void reverse_GEMM(matrix::Matrix& OFmap , Tensor4& result)
+Tensor4 treatment_fast(Tensor4& f_map , Tensor4& filter)
 {
-	for (int butch = 0 ; butch < result.get_num_butch() ; ++butch)
-		for (int chan = 0 ; chan < result.get_num_mrx() ; ++chan)
-			for (int i = 0 ; i < result.get_high() ; ++i)
-				for (int j = 0 ; j < result.get_width() ; ++j)
-					result[butch][chan][i][j] = OFmap[butch * result.get_high() * result.get_width() + i * result.get_width() + j][chan];
-}
-
-Tensor4 conv_layer_fast(Tensor4& f_map , Tensor4& filter)
-{
-
 	int new_h = f_map.get_high() - filter.get_high() + 1;
 	int new_w = f_map.get_width() - filter.get_width() + 1;
-
 	Tensor4 result(f_map.get_num_butch() , filter.get_num_butch() , new_h , new_w);
 
-	matrix::Matrix IFmap = GEMM_ten(f_map , filter);
-	matrix::Matrix fil_matrix = GEMM_ker(filter);
-
-	matrix::Matrix OFmap(IFmap.get_num_str() , fil_matrix.get_num_col());
-
-	//std::cout << IFmap << std::endl << fil_matrix << std::endl;
-
-	matrix::Matrix_product_fast(IFmap , fil_matrix , OFmap);
-
-	//std::cout << OFmap;
-
-	reverse_GEMM(OFmap , result);
-
+	for (int butch = 0 ; butch < f_map.get_num_butch() ; ++butch)
+	{
+		for (int ker = 0 ; ker < filter.get_num_butch() ; ++ker)
+		{
+			make_chan_fast(f_map[butch] , filter[ker] , result[butch][ker]);
+		}
+	}
 	return result;
+}
+
+void make_chan_fast(Chanel& chanel , Chanel& kernel , matrix::Matrix& result)
+{
+	for (int chan = 0 ; chan < kernel.get_num_mrx() ; ++chan)
+	{
+		matrix::Matrix res_per(result.get_num_str() , result.get_num_col());
+		for (int i = 0 ; i < res_per.get_num_str() ; ++i)
+			for (int j = 0 ; j < res_per.get_num_col() ; ++j)
+			{
+				matrix::Matrix per(chanel[chan] , kernel[chan].get_num_str() , kernel[chan].get_num_col() , i , j);
+				res_per[i][j] = matrix::elem_sum(per , kernel[chan]);
+			}
+		matrix::sum(result , res_per , result);
+	}
+}
+
+std::vector<Tensor4> make_parts_for_tile(Tensor4& f_map , int filter_high)
+{
+	std::vector<Tensor4> tile_parts;
+	for (int i = 0 ; i < f_map.get_high() - filter_high + 1 ; ++i)
+		tile_parts.emplace_back(i , filter_high , f_map);
+
+	return tile_parts;
 }
 
 
--- VladislavLoznovenko/task_conv/Convolutions_h/Conv_fast.h
+++ VladislavLoznovenko/task_conv/Convolutions_h/Conv_fast.h
@@ -3,7 +3,8 @@
 
 using namespace Tensor;
 
-matrix::Matrix GEMM_ker(Tensor4& filter);
-matrix::Matrix GEMM_ten(Tensor4& f_map , Tensor4& filter);
-void reverse_GEMM(matrix::Matrix& OFmap , Tensor4& result);
-Tensor4 conv_layer_fast(Tensor4& f_map , Tensor4& filter);
+Tensor4 conv_layer_fast(Tensor4& for_conv , Tensor4& filter);
+Tensor4 treatment_fast(Tensor4& f_map , Tensor4& filter);
+void treat(Tensor4& ten_part , Tensor4& result , Tensor4& filter , int counter);
+void make_chan_fast(Chanel& chanel , Chanel& kernel , matrix::Matrix& result);
+std::vector<Tensor4> make_parts_for_tile(Tensor4& f_map , int filter_high);
