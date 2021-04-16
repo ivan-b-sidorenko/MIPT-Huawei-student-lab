1ce00d11c7e4a76c91ddb15ada6920f3c45ac344
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-04-17 02:07:49 +0300

    added Winograd convolution but slow

--- VladislavLoznovenko/task_conv/main.cpp
+++ VladislavLoznovenko/task_conv/main.cpp
@@ -1,6 +1,7 @@
 #include "Convolutions_h/Conv_naiv.h"
 #include "Convolutions_h/Conv_fast.h"
 #include "Convolutions_h/Conv_GEMM.h"
+#include "Convolutions_h/Conv_Vinograd.h"
 #include "time.h"
 
 using namespace matrix;
@@ -12,10 +13,13 @@ int main()
 	uint64_t duration_normal = 0;
 	uint64_t duration_fast = 0;
 	uint64_t duration_GEMM = 0;
+	uint64_t duration_WIN = 0;
 	my_time::Timer clock;
 
-	Tensor4 test(1 , 3 , 100 , 50 , 0);
-	Tensor4 filter(16 , 3 , 4 , 4 , 0);
+	Tensor4 test(1 , 3 , 101 , 100 , 0);
+	Tensor4 filter(3 , 3 , 3 , 3 , 0);
+	std::vector<Matrix> Win = Win_mrx();
+
 	for (int i = 0 ; i < 5 ; ++i)
 	{
 		clock.restart();
@@ -30,13 +34,19 @@ int main()
 		Tensor4 res_3 = conv_layer_GEMM(test , filter);
 		duration_GEMM += clock.get_mls();
 
+		clock.restart();
+		Tensor4 res_4 = convolution_layer_Win(test , filter , Win);
+		duration_WIN += clock.get_mls();
+
 		assert(res_1 == res_2);
 		assert(res_1 == res_3);
+		assert(res_1 == res_4);
 	}
 
-	std::cout << "Normal conv - " << duration_normal << " - milliseconds" << std::endl;
-	std::cout << "Fast conv   - " << duration_fast << " - milliseconds" << std::endl;
-	std::cout << "GEMM conv   - " << duration_GEMM << " - milliseconds" << std::endl;
+	std::cout << "Normal conv - " << duration_normal / 5 << " - milliseconds" << std::endl;
+	std::cout << "Fast conv   - " << duration_fast / 5 << " - milliseconds" << std::endl;
+	std::cout << "GEMM conv   - " << duration_GEMM / 5 << " - milliseconds" << std::endl;
+	std::cout << "WIN conv   - " << duration_WIN / 5 << " - milliseconds" << std::endl;
 
 	//std::cout << "test\n" << test << std::endl;
 	//std::cout << "filter\n" << filter << std::endl;
