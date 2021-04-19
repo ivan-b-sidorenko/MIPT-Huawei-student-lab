09061de35804a082ea0181f59c48565fe6cef851
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-04-19 22:11:07 +0300

    Made Winograd faster

--- VladislavLoznovenko/task_conv/Convolutions_h/Conv_Vinograd.h
+++ VladislavLoznovenko/task_conv/Convolutions_h/Conv_Vinograd.h
@@ -4,13 +4,15 @@
 using namespace Tensor;
 using namespace matrix;
 
-#define BLOCK_SIZE 2
-#define KER_SIZE 3
+//BLOCK_SIZE 2
+//KER_SIZE 3
 
 enum{ A , AT , B , BT , G , GT };
 
 std::vector<Matrix> Win_mrx();
-Tensor4 convolution_layer_Win(Tensor4& for_conv , Tensor4& filter , std::vector<Matrix>& convert);
-void treatment_Win(Chanel& chanel , Tensor4& kernels , Chanel& result , std::vector<Matrix>& convert);
-void make_chan_Win(Chanel& chanel , Chanel& kernel , Matrix& result , std::vector<Matrix>& convert);
-void Win_formule(Matrix& block , Matrix kernel , std::vector<Matrix>& convert);
+Tensor4 convolution_layer_Win(Tensor4& for_conv , Tensor4& filter);
+void treatment_Win(Chanel& chanel , Tensor4& kernels , Chanel& result);
+void make_chan_Win(Chanel& chanel , Chanel& kernel , Matrix& result);
+Matrix BtdB(Matrix& block);
+Matrix GgGt(Matrix& kernel);
+Matrix AresAt(Matrix& res);
